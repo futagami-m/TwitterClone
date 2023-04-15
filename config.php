ec5e33dc2ad9c5abf903ad616fa684e498f23592
futagami-m <hana20190406@gmail.com>
2023-04-15 17:58:20 +0900

    会員登録機能

--- config.php
+++ config.php
@@ -6,4 +6,9 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL', '/TwitterClone/');
 
+// データベースの接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'maitter');
 ?>
