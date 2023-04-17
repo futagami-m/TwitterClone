90c40e4d5abbb8788c7e765bbfaa428710ad3a45
futagami-m <hana20190406@gmail.com>
2023-04-17 16:25:01 +0900

    ログイン関数追加

--- Views/common/head.php
+++ Views/common/head.php
@@ -6,7 +6,7 @@
 
     <link rel="stylesheet" href="<?php echo HOME_URL; ?>Views/css/stylesheet.css">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo HOME_URL; ?>Views/css/stylesheet.css">
     <!-- JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
