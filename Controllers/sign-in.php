ec55272b963297a778ab5c3767d4e2c72e0e0100
futagami-m <hana20190406@gmail.com>
2023-04-17 21:56:59 +0900

    ログイン機能

--- Controllers/sign-in.php
+++ Controllers/sign-in.php
@@ -17,12 +17,12 @@ $try_login_result = null;
 // メールアドレスとパスワードが入力されている場合
 if (isset($_POST['email']) && isset($_POST['password'])) {
     // ログインチェック実行
-    $user = findUserAndCheckPassword($_POST['email'],$_POST['password']);
+    $user = findUserAndCheckPassword($_POST['email'], $_POST['password']);
  
     // ログインに成功した場合
     if ($user) {
         // ユーザー情報をセッションに保存
- 
+        saveUserSession($user);
  
         // ホーム画面へ遷移
         header('Location: ' . HOME_URL . 'Controllers/home.php');
