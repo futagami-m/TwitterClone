c17241f9ce0bf1119aab0af3daad7faab17289a0
futagami-m <hana20190406@gmail.com>
2023-04-18 12:03:14 +0900

    同感！機能を非同期で実装

--- Views/js/likes.js
+++ Views/js/likes.js
@@ -6,28 +6,57 @@ $(function () {
     // 同感！がクリックされたとき
     $('.js-like').click(function () {
         const this_obj = $(this);
+        const tweet_id = $(this).data('tweet-id');
         const like_id = $(this).data('like-id');
         const like_count_obj = $(this).parent().find('.js-like-count');
         let like_count = Number(like_count_obj.html());
  
         if (like_id) {
             // 同感！取り消し
-            // 同感！カウントを減らす
-            like_count--;
-            like_count_obj.html(like_count);
-            this_obj.data('like-id', null);
+            // 非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'like_id': like_id
+                },
+                timeout: 10000
+            })
+                // 取り消しが成功
+                .done(() => {
+                    // 同感！カウントを減らす
+                    like_count--;
+                    like_count_obj.html(like_count);
+                    this_obj.data('like-id', null);
  
-            // 同感！ボタンの色をグレーに変更
-            $(this).find('img').attr('src', '../Views/img/icon-heart.svg');
-        } else {
+                    //  同感！ボタンの色をグレーに変更
+                    $(this).find('img').attr('src', '../Views/img/icon-heart.svg');
+                })
+                .fail((data) => {
+                    alert('処理中にエラーが発生しました。');
+                    console.log(data);
+                });
+            } else {
             // 同感！付与
+            // 非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'tweet_id': tweet_id
+                },
+                timeout: 10000
+            })
+            // 同感！が成功
+            .done((data) => {
             // 同感！カウントを増やす
-            like_count++;
-            like_count_obj.html(like_count);
-            this_obj.data('like-id', true);
+                like_count++;
+                like_count_obj.html(like_count);
+                this_obj.data('like-id', data['like_id']);
  
             // 同感！ボタンの色を青に変更
             $(this).find('img').attr('src', '../Views/img/icon-heart-twitterblue.svg');
-        }
+        });
+    }
     });
 })
