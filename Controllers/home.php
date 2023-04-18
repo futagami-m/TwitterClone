15e33c209c513ca5fabacdb49366e024a146fdd4
futagami-m <hana20190406@gmail.com>
2023-04-18 10:34:22 +0900

    ホーム画面

--- Controllers/home.php
+++ Controllers/home.php
@@ -8,7 +8,11 @@ include_once '../config.php';
 // 便利な関数を読み込み
 include_once '../util.php';
 
-//TODO:ログインチェック
+// ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
+
+
+//ログインチェック
 $user = getUserSession();
 if (!$user) {
     // ログインしていない
@@ -18,35 +22,9 @@ if (!$user) {
 // 表示用の変数
 $view_user = $user;
 
+// マ、いーか一覧
 
-
-///////////////////////////////////////
-// マいーか一覧
-///////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => 'さっきしょうもないミスをしてしまいました。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2023-03-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースで反省中！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2023-03-10 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
+$view_tweets = findTweets($user);
 
 //画面表示
 include_once '../Views/home.php';
