835d1c5f72828477c7b391ecf58625f6b3cbc04e
futagami-m <hana20190406@gmail.com>
2023-04-18 15:03:22 +0900

    ホーム画面完成

--- Controllers/home.php
+++ Controllers/home.php
@@ -10,6 +10,9 @@ include_once '../util.php';
 
 // ツイートデータ操作モデルを読み込む
 include_once '../Models/tweets.php';
+// フォローデータ操作モデルを読み込む
+include_once '../Models/follows.php';
+
 
 
 //ログインチェック
@@ -19,12 +22,17 @@ if (!$user) {
     header('Location: ' . HOME_URL . 'Controllers/sign-in.php');
     exit;
 }
+//自分がナカーマにしているユーザーID一覧を取得
+$following_user_ids = findFollowingUserIds($user['id']);
+//自分のツイートも表示するために自分のIDも追加
+$following_user_ids[] = $user['id'];
+
 // 表示用の変数
 $view_user = $user;
 
 // マ、いーか一覧
 
-$view_tweets = findTweets($user);
+$view_tweets = findTweets($user, null, $following_user_ids);
 
 //画面表示
 include_once '../Views/home.php';
